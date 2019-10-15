d6dc56497e5e472c5b6f037c847943406b3f77ca
asheshGlooko <ashesh1105@gmail.com>
2019-10-15 12:06:30 -0700

    Updated comments in Optional Project

--- 02_macOS_CommandLineTools_Project/Optionals/Optionals/main.swift
+++ 02_macOS_CommandLineTools_Project/Optionals/Optionals/main.swift
@@ -1,20 +1,12 @@
-//
-//  main.swift
-//  Optionals
-//
-//  Created by Ashesh Singh on 9/15/19.
-//  Copyright © 2019 Ashesh Singh. All rights reserved.
-//
-
 import Foundation
 
 /*
  Optionals help prevent iOS apps from crashing.
  When we unpack a data and typecast to another data type, Swift allows us to handle that situation in 4 different ways:
  1) ? => Optional, where we speficy that there are chances data in one type may not be as we need it, e.g., characters instead of numbers,
- 2) ! => We force unwrap and tell Swift that we know the data we are unwraping is how we think it is, so do it.
+ 2) ! => We force unwrap and tell Swift that we know the data we are unwraping is how we think it is, so go ahead and do it.
  3) ?? <default value> => Here we speficy a default value in case the data is not the way we need it, e.g. characters instead of numbers,
- 4) if let - else => Here we secify code incase data is the desired one, else handle the error
+ 4) if let - else => Here we specify code to run incase data is the desired one, else part handles the error
  In abobe, 1) and 2) do not help app from crashing if we get surprizes but 3) and 4) do
  */
 
@@ -23,8 +15,8 @@ import Foundation
 //myName.uppercased()
 
 // This is how you can take care of above situation. You will get nil results but app won't crash
-var myName : String?
-print(myName?.uppercased()) // prints nill with a warning: "Expression implicitly coerced from 'String?' to 'Any'"
+var myName : String?    // ? with datatype is needed so we can use optional in the line below. This is called Optional Chaining
+print(myName?.uppercased()) // prints nil with a warning: "Expression implicitly coerced from 'String?' to 'Any'"
 
 // 2) Where you're certain the underlying data is correct (like Int, for example), you can force unwrap using ! operator
 // Note here that if num1 did not has number in there (e.g., "ce"), app will crash
@@ -37,7 +29,7 @@ print(product)  // prints 25
 var myAge = "rr"
 // If you remove ?? default operator below, you get an error: "Value of optional type 'Int?' must be unwrapped to a value of type 'Int'"
 var catAge = (Int(myAge) ?? 0) * 7
-print(catAge)   // prints 0 but no error and app does not crash
+print(catAge)   // prints 0 with no error and app does not crash
 
 // 4) if let example
 // In below example, app won't crash and prompts the user to enter a number, so we can use this to compute the results or do error handling too!
